89976d7d76b753919bb5774639629c55fdebd7a6
kirti sharma <kirti.sharma@successive.tech>
2023-09-07 11:22:39 +0530

    url typing error fix

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,6 +5,6 @@ const productController = require('../controllers/productController');
 
 // Define your API routes
 router.get('/api/products', productController.getProducts);
-router.get('/api/products/:id', productController.getProductById);
+router.get('/api/product/:id', productController.getProductById);
 
 module.exports = router;
